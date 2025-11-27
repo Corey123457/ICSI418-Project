215f3c24c2ec051c4f4ad763ac8d8c5beec4ee77
Corey123457 <142933963+Corey123457@users.noreply.github.com>
2025-11-27 06:31:02 -0500

    Delete Soruce Code directory

--- Soruce Code/Server Code/Pref.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const PreferenceSchema = new mongoose.Schema({
-    pref: String
-});
-
-const Preference = mongoose.model("Preference", PreferenceSchema);
-
-module.exports = Preference;
