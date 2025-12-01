04e07e1672af1cf7982cb7e9f66b27bb3821edaf
Corey123457 <142933963+Corey123457@users.noreply.github.com>
2025-11-30 20:00:56 -0500

    Add files via upload

---
+++ server/Pref.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const PreferenceSchema = new mongoose.Schema({
+    pref: [String],
+    time: String
+});
+
+const Preference = mongoose.model("Preference", PreferenceSchema);
+
+module.exports = Preference;
